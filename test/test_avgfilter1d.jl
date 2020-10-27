910b7a48682f23c5fa8489817e01942358a9c300
Takafumi Arakaki <aka.tkf@gmail.com>
2020-10-26 17:30:02 -0700

    Remove debugging statements

--- test/test_avgfilter1d.jl
+++ test/test_avgfilter1d.jl
@@ -4,8 +4,6 @@ using TapirBenchmarks
 using Test
 
 @testset for n in [3, 5, 100, 2^10]
-# @testset for n in [3, 5]
-    @show n
     ys0, xs = avgfilter1d_setup(n)
     ys1 = avgfilter1d_seq!(copy(ys0), xs)
     @test avgfilter1d_threads!(copy(ys0), xs) == ys1
